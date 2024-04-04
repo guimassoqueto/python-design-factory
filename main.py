30e2c673c575f39a2fc7198f7f01b51393c109aa
guimassoqueto <gui.massoqueto@gmail.com>
2024-04-04 19:28:16 -0300

    feat: update main function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from app.a_simple_factory.main import run
+from app.b_factory_method.main import run
 
 
 if __name__ == "__main__":
-    run() # SIMPLE FACTORY PATTERN
+    # run() # SIMPLE FACTORY PATTERN
+    # run() # FACTORY METHOD PATTERN
+    pass
