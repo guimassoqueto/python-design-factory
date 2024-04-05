d73f74e7a39af3c27b1f577951c4a652bf974790
guimassoqueto <gui.massoqueto@gmail.com>
2024-04-05 11:24:34 -0300

    docs: add abstract_factory image

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from app.c_abstract_factory.main import run as abstract_factory_run
 if __name__ == "__main__":
     # simple_factory_run() # SIMPLE FACTORY PATTERN
     # factory_method_run() # FACTORY METHOD PATTERN
-    # abstract_factory_run() # ABSTRACT FACTORY PATTERN
+    abstract_factory_run() # ABSTRACT FACTORY PATTERN
     pass
