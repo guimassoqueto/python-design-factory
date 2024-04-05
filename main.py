3b3f1689e48339f8055ca712d8f33c0b9f3a3811
guimassoqueto <gui.massoqueto@gmail.com>
2024-04-05 08:59:02 -0300

    chore: update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from app.a_simple_factory.main import run
-from app.b_factory_method.main import run
+from app.a_simple_factory.main import run as simple_factory_run
+from app.b_factory_method.main import run as factory_method_run
+from app.c_abstract_factory.main import run as abstract_factory_run
 
 
 if __name__ == "__main__":
-    # run() # SIMPLE FACTORY PATTERN
-    # run() # FACTORY METHOD PATTERN
+    # simple_factory_run() # SIMPLE FACTORY PATTERN
+    # factory_method_run() # FACTORY METHOD PATTERN
+    # abstract_factory_run() # ABSTRACT FACTORY PATTERN
     pass
